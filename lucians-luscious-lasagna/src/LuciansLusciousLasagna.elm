f11857f935bd254352f12781f120a5dd0ec3d3a7
mrdavid28 <dawidpotczyk@gmail.com>
2024-01-06 02:05:29 +0100

    marios-marvellous-lasagna

---
+++ lucians-luscious-lasagna/src/LuciansLusciousLasagna.elm
@@ -0,0 +1,14 @@
+module LuciansLusciousLasagna exposing (expectedMinutesInOven, preparationTimeInMinutes)
+
+-- TODO: define the expectedMinutesInOven constant
+expectedMinutesInOven = 40
+
+-- TODO: define the preparationTimeInMinutes function
+{-Define preparationTimeInMinutes that takes the number of layers 
+in the lasagna as a parameter and returns how many minutes it takes to prepare the lasagna,
+ assuming each layer takes 2 minutes to prepare.-}
+
+preparationTimeInMinutes number_of_layers = number_of_layers * 2
+-- TODO: define the elapsedTimeInMinutes function
+
+    
