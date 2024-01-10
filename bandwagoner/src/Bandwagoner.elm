036aaf2c509067c7d404166f1a07e06baf999848
mrdavid28 <dawidpotczyk@gmail.com>
2024-01-10 01:27:48 +0100

    Initial Commit

---
+++ bandwagoner/src/Bandwagoner.elm
@@ -0,0 +1,36 @@
+module Bandwagoner exposing (..)
+
+-- TODO: please define a type alias for the 'Coach' record
+type alias Coach =
+    { name : String
+    , formerPlayer : Bool
+    } 
+-- TODO: please define the 'Stats' record
+type alias Stats =
+    { wins : Int
+    , losses : Int
+    } 
+-- TODO: please define the 'Team' record
+type alias Team =
+    { name : String
+    , coach : Coach
+    , stats : Stats
+    } 
+replaceCoach : Coach -> Team -> Team
+replaceCoach newCoach team =
+    {team | coach = newCoach}
+
+createTeam : String -> Stats -> Coach -> Team
+createTeam name stats coach =
+    Team name coach stats
+
+
+
+-- TODO: use an extensible record to accept any record that has a `stats: Stats` field
+-- TODO: use pattern matching in the function parameter to get the `stats` field from the record
+
+
+
+rootForTeam : {a | stats : Stats } -> Bool
+rootForTeam team = 
+    team.stats.wins > team.stats.losses
